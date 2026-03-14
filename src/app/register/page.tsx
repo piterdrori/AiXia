019487108946eae22b4e9a8613b54a3e317b5c41
piterdrori <Piter@aixia.io>
2026-03-14 18:54:41 +0800

    Update page.tsx

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, } from "react-router-dom";
 import { supabase } from "@/lib/supabase";
 import { v4 as uuidv4 } from "uuid";
 import { sendVerificationEmail } from "@/lib/sendVerificationEmail";

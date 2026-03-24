b882d14aec9ca65727871b919f4eec003cb2f113
piterdrori <Piter@aixia.io>
2026-03-24 09:11:10 +0800

    Create useRequest.ts

---
+++ src/lib/useRequest.ts
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import {
+  createInitialState,
+  startLoading,
+  setSuccess,
+  setError,
+  RequestState,
+} from "./requestState";
+
+export function useRequest<T = any>() {
+  const [state, setState] = useState<RequestState<T>>(createInitialState());
+
+  const run = async (fn: () => Promise<T>) => {
+    setState((prev) => startLoading(prev));
+
+    try {
+      const result = await fn();
+      setState(setSuccess(result));
+      return result;
+    } catch (err: any) {
+      const message = err?.message || "Something went wrong";
+      setState(setError(message));
+      throw err;
+    }
+  };
+
+  return {
+    ...state,
+    run,
+    setState,
+  };
+}

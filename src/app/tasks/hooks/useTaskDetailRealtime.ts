66539f9d0a5c04f92b002f180c870873feab6e5a
piterdrori <Piter@aixia.io>
2026-04-02 07:38:34 +0800

    Create useTaskDetailRealtime.ts

---
+++ src/app/tasks/hooks/useTaskDetailRealtime.ts
@@ -0,0 +1,47 @@
+import { useEffect } from "react";
+import {
+  subscribeToTask,
+  subscribeToTaskComments,
+  subscribeToTaskActivity,
+  removeRealtimeChannel,
+} from "@/lib/realtime";
+import { TaskRow, TaskCommentRow, TaskActivityRow } from "../lib/task.types";
+
+interface UseTaskDetailRealtimeProps {
+  taskId: string | undefined;
+  onTaskUpdate: (updatedTask: Partial<TaskRow>) => void;
+  onCommentInsert: (newComment: TaskCommentRow) => void;
+  onActivityInsert: (newActivity: TaskActivityRow) => void;
+}
+
+export function useTaskDetailRealtime({
+  taskId,
+  onTaskUpdate,
+  onCommentInsert,
+  onActivityInsert,
+}: UseTaskDetailRealtimeProps) {
+  useEffect(() => {
+    if (!taskId) return;
+
+    subscribeToTask({
+      taskId,
+      onUpdate: onTaskUpdate,
+    });
+
+    subscribeToTaskComments({
+      taskId,
+      onInsert: onCommentInsert,
+    });
+
+    subscribeToTaskActivity({
+      taskId,
+      onInsert: onActivityInsert,
+    });
+
+    return () => {
+      removeRealtimeChannel(`task:${taskId}`);
+      removeRealtimeChannel(`task:comments:${taskId}`);
+      removeRealtimeChannel(`task:activity:${taskId}`);
+    };
+  }, [taskId, onTaskUpdate, onCommentInsert, onActivityInsert]);
+}

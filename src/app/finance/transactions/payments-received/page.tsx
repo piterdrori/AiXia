3dead56a86778f82a26e8186c4913a5c9e48613f
piterdrori <Piter@aixia.io>
2026-04-17 15:46:01 +0800

    Update page.tsx

--- src/app/finance/transactions/payments-received/page.tsx
+++ src/app/finance/transactions/payments-received/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { getPaymentsReceived } from "@/lib/finance/paymentsReceived";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/router";
 
 type PaymentRow = {
   id: string;

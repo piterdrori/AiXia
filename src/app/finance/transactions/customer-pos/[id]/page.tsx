4c7d6374a64a62816b9eae9a94420844dd0cb04f
piterdrori <Piter@aixia.io>
2026-04-27 15:45:05 +0800

    Update page.tsx

--- src/app/finance/transactions/customer-pos/[id]/page.tsx
+++ src/app/finance/transactions/customer-pos/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import {
   Archive,

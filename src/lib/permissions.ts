45792db33579c4f54ff073b5d277f53b4af8f3fd
piterdrori <Piter@aixia.io>
2026-05-02 07:02:11 +0800

    Update permissions.ts

--- src/lib/permissions.ts
+++ src/lib/permissions.ts
@@ -725,9 +725,9 @@ const ROUTE_PERMISSIONS: Record<string, RoutePermission> = {
   "/finance/expenses/new": { permission: "createExpenses" },
   "/finance/expenses/:id": { permission: "viewExpenses" },
 
-  "/finance/transactions/expenses": { permission: "viewExpenses" },
-  "/finance/transactions/expenses/new": { permission: "createExpenses" },
-  "/finance/transactions/expenses/:id": { permission: "viewExpenses" },
+  "/finance/transactions/expenses-payments-made": { permission: "viewFinance" },
+  "/finance/transactions/expenses-payments-made/new": { permission: "recordPaymentsMade" },
+  "/finance/transactions/expenses-payments-made/:id": { permission: "viewFinance" },
 
   "/finance/reimbursements": { permission: "viewReimbursements" },
   "/finance/reimbursements/:id": { permission: "viewReimbursements" },

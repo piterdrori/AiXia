abed52f55023bf6885e325feba71773cfa4af08e
piterdrori <Piter@aixia.io>
2026-05-07 13:29:45 +0800

    Create index.ts

---
+++ src/components/aixia/index.ts
@@ -0,0 +1,6 @@
+export { AixiaBadge } from "./AixiaBadge";
+export { AixiaButton } from "./AixiaButton";
+export { AixiaHero } from "./AixiaHero";
+export { AixiaMetricCard } from "./AixiaMetricCard";
+export { AixiaPage } from "./AixiaPage";
+export { AixiaSection } from "./AixiaSection";

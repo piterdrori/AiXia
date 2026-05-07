3986ab47ba65d8b8ee120f78e214d02985ee2008
piterdrori <Piter@aixia.io>
2026-05-07 18:03:19 +0800

    Delete src/components/aixia directory

--- src/components/aixia/index.ts
+++
@@ -1,8 +0,0 @@
-export { AixiaBadge } from "./AixiaBadge";
-export { AixiaButton } from "./AixiaButton";
-export { AixiaHero } from "./AixiaHero";
-export { AixiaMetricCard } from "./AixiaMetricCard";
-export { AixiaModal } from "./AixiaModal";
-export { AixiaPage } from "./AixiaPage";
-export { AixiaSection } from "./AixiaSection";
-export { AixiaSortableHeader, AixiaTableShell } from "./AixiaTable";

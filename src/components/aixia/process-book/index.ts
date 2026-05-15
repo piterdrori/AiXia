6fb8661db7be1a08b51526056f3e42372d8bcf05
piterdrori <Piter@aixia.io>
2026-05-15 18:17:25 +0800

    Update index.ts

--- src/components/aixia/process-book/index.ts
+++ src/components/aixia/process-book/index.ts
@@ -1,11 +1,6 @@
-export {
-  AixiaProcessBook,
-  type AixiaProcessBookProps,
-  type AixiaProcessStageItem,
-  type AixiaProcessStageStatus,
-  type AixiaProcessSummaryItem,
-} from "./AixiaProcessBook";
-export { AixiaProcessNavigation, type AixiaProcessNavigationProps } from "./AixiaProcessNavigation";
-export { AixiaProcessStage, type AixiaProcessStageProps } from "./AixiaProcessStage";
-export { AixiaProcessStepper, type AixiaProcessStepperProps } from "./AixiaProcessStepper";
-export { AixiaProcessSummaryPanel, type AixiaProcessSummaryPanelProps } from "./AixiaProcessSummaryPanel";
+export * from "./AixiaProcessBook";
+export * from "./AixiaProcessStepper";
+export * from "./AixiaProcessStage";
+export * from "./AixiaProcessSummaryPanel";
+export * from "./AixiaProcessNavigation";
+export * from "./AixiaProcessInfo";

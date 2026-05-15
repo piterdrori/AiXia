31173e542488f8a196b7b7c9b0f13550323ea505
piterdrori <Piter@aixia.io>
2026-05-15 16:07:11 +0800

    Add files via upload

--- src/components/aixia/index.ts
+++ src/components/aixia/index.ts
@@ -1,92 +1,53 @@
-export { AixiaAccessRule } from "./AixiaAccessRule";
-export { AixiaActionCard } from "./AixiaActionCard";
-export { AixiaActionSystem } from "./AixiaActionSystem";
-export { AixiaAlert } from "./AixiaAlert";
-export { AixiaArchiveManagerModal } from "./AixiaArchiveManagerModal";
-export { AixiaBadge } from "./AixiaBadge";
-export { AixiaButton } from "./AixiaButton";
-export { AixiaChildAllocationRegistry } from "./AixiaChildAllocationRegistry";
-export { AixiaCurrencyBadge } from "./AixiaCurrencyBadge";
-export { AixiaDefaultBadge } from "./AixiaDefaultBadge";
-export {
-  AixiaDocumentUploadPanel,
-  type AixiaDocumentUploadAttachment,
-} from "./AixiaDocumentUploadPanel";
-export { AixiaHero } from "./AixiaHero";
-export { AixiaInfoBlock } from "./AixiaInfoBlock";
-export { AixiaMetricCard } from "./AixiaMetricCard";
-export { AixiaMetricGrid } from "./AixiaMetricGrid";
-export { AixiaModal } from "./AixiaModal";
-export { AixiaPage } from "./AixiaPage";
-export { AixiaEmptyState } from "./AixiaEmptyState";
-export { AixiaEmployeeIdentityCell } from "./AixiaEmployeeIdentityCell";
-export { AixiaSearchField } from "./AixiaSearchField";
-export { AixiaRegistryToolbar } from "./AixiaRegistryToolbar";
-export { AixiaSection } from "./AixiaSection";
-export { AixiaSmartGrid } from "./AixiaSmartGrid";
-export { AixiaSmartLayout } from "./AixiaSmartLayout";
-export { AixiaStatusBadge } from "./AixiaStatusBadge";
-export { AixiaStatusCard } from "./AixiaStatusCard";
-export { AixiaFeaturePanel } from "./AixiaFeaturePanel";
-export { AixiaSignalRow } from "./AixiaSignalRow";
-export { AixiaSideList, AixiaSideListRow } from "./AixiaSideList";
-export { AixiaValueBlock } from "./AixiaValueBlock";
-export { AixiaWorkspaceCard } from "./AixiaWorkspaceCard";
-export {
-  AixiaNavigationCard,
-  AixiaNavigationGrid,
-  AixiaNavigationInfoPanel,
-  AixiaNavigationStatBlock,
-} from "./AixiaNavigationCard";
-export { AixiaSelectableTile } from "./AixiaSelectableTile";
-export { AixiaEntityCard } from "./AixiaEntityCard";
-export { AixiaProfileCard } from "./AixiaProfileCard";
-export { AixiaHistoryRow } from "./AixiaHistoryRow";
-export {
-  AixiaCheckboxField,
-  AixiaDisplayBlock,
-  AixiaFieldLabel,
-  AixiaFormField,
-  AixiaFormFullWidth,
-  AixiaFormGrid,
-  AixiaFormRowCard,
-  AixiaInputField,
-  AixiaSelectField,
-  AixiaTextareaField,
-} from "./AixiaFormFields";
-export { AixiaDetailSection } from "./AixiaDetailSection";
-export {
-  AixiaAccessDeniedState,
-  AixiaLoadingState,
-  AixiaNotFoundState,
-  AixiaPageState,
-} from "./AixiaPageStates";
-export { AixiaActionStack, AixiaAlertText } from "./AixiaContentBlocks";
-export { AixiaReviewBlock, AixiaReviewGrid } from "./AixiaReviewBlocks";
-export { AixiaSortableHeader, AixiaTableShell } from "./AixiaTable";
-export {
-  AixiaTableActionsCell,
-  AixiaTableBadgeCell,
-  AixiaTableDateCell,
-  AixiaTableTextCell,
-} from "./AixiaTableCells";
-export {
-  AixiaFinancePrintBankBlock,
-  AixiaFinancePrintBottomGrid,
-  AixiaFinancePrintFooter,
-  AixiaFinancePrintHeader,
-  AixiaFinancePrintLineTable,
-  AixiaFinancePrintPartyBlock,
-  AixiaFinancePrintSection,
-  AixiaFinancePrintSheet,
-  AixiaFinancePrintTermsBlock,
-  AixiaFinancePrintTotalsBlock,
-  aixiaPrintHasValue,
-  aixiaPrintJoin,
-  type AixiaFinancePrintBankRow,
-  type AixiaFinancePrintLineItem,
-  type AixiaFinancePrintMetaRow,
-  type AixiaFinancePrintParty,
-  type AixiaFinancePrintTermRow,
-  type AixiaFinancePrintTotalRow,
-} from "./AixiaFinancePrint";
+import type { ReactNode } from "react";
+
+import { AixiaButton } from "../AixiaButton";
+
+export type AixiaProcessNavigationProps = {
+  onPrevious?: () => void;
+  onNext?: () => void;
+  onSaveDraft?: () => void;
+  finalAction?: ReactNode;
+  previousDisabled?: boolean;
+  nextDisabled?: boolean;
+  saveDisabled?: boolean;
+  previousLabel?: string;
+  nextLabel?: string;
+  saveLabel?: string;
+};
+
+export function AixiaProcessNavigation({
+  onPrevious,
+  onNext,
+  onSaveDraft,
+  finalAction,
+  previousDisabled,
+  nextDisabled,
+  saveDisabled,
+  previousLabel = "Previous",
+  nextLabel = "Next",
+  saveLabel = "Save Draft",
+}: AixiaProcessNavigationProps) {
+  return (
+    <footer className="aixia-process-navigation" aria-label="Process navigation">
+      <div className="aixia-process-navigation__group">
+        <AixiaButton type="button" variant="secondary" onClick={onPrevious} disabled={previousDisabled}>
+          {previousLabel}
+        </AixiaButton>
+      </div>
+
+      <div className="aixia-process-navigation__group">
+        {onSaveDraft ? (
+          <AixiaButton type="button" variant="secondary" onClick={onSaveDraft} disabled={saveDisabled}>
+            {saveLabel}
+          </AixiaButton>
+        ) : null}
+        {onNext ? (
+          <AixiaButton type="button" variant="primary" onClick={onNext} disabled={nextDisabled}>
+            {nextLabel}
+          </AixiaButton>
+        ) : null}
+        {finalAction}
+      </div>
+    </footer>
+  );
+}

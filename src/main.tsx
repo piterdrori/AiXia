ee87614dde0961e546ba5c93838940fa12dd7f59
piterdrori <Piter@aixia.io>
2026-05-15 16:20:21 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./styles/aixia-design-system.css";
+import "./styles/aixia-process-book.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
